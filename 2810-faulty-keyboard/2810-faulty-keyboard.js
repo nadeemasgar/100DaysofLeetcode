b33d8200180b977f741a208e052075daaba5dedb
MD NADEEM ASGAR <43141745+nadeemasgar@users.noreply.github.com>
2024-04-01 23:09:55 +0530

    Time: 116 ms (28.60%), Space: 53.8 MB (89.25%) - LeetHub

---
+++ 2810-faulty-keyboard/2810-faulty-keyboard.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var finalString = function(s) {
+    const res = [];
+    for(let i = 0; i < s.length; i++) {  //  for (char of s)
+        const ch = s.charAt(i); // const ch = s[i]
+        if(ch !== 'i') {
+            res.push(ch);
+        }
+        else {
+            res.reverse();
+        }
+    }
+    
+    return res.join('');
+};
+
+/*
+Property acess - let char = text[0];
+charAt()	It provides the char value present at the specified index.
+concat()	It provides a combination of two or more strings.
+indexOf()	It provides the position of a char value present in the given string.
+lastIndexOf()	It provides the position of a char value present in the given string by searching a character from the last position.
+search()	It searches a specified regular expression in a given string and returns its position if a match occurs.
+substring()	It is used to fetch the part of the given string on the basis of the specified index.
+split()	It splits a string into substring array, then returns that newly created array.
+trim()	It trims the white space from the left and right side of the string.
+*/
