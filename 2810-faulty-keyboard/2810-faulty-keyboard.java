ac6409dd7d7e5dcb5ee029d7124f12de88f79ef9
MD NADEEM ASGAR <43141745+nadeemasgar@users.noreply.github.com>
2024-04-01 22:45:30 +0530

    Time: 3 ms (98.23%), Space: 44.7 MB (61.27%) - LeetHub

--- 2810-faulty-keyboard/2810-faulty-keyboard.java
+++ 2810-faulty-keyboard/2810-faulty-keyboard.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
             if(ch != 'i') {
-                sb.append(ch);
+                sb.append(ch); 
             }
             else {
                 sb.reverse();
@@ -14,3 +14,27 @@ class Solution {
         return sb.toString();
     }
 }
+
+/*
+String Methods ->
+- charAt()	Returns the character at the specified index (position)
+- compareTo()	Compares two strings lexicographically
+- concat()	Appends a string to the end of another string
+- equals()	Compares two strings. Returns true if the strings are equal, and false if not
+- valueOf()	Returns the string representation of the specified value
+- trim()	Removes whitespace from both ends of a string
+- length()	Returns the length of a specified string
+- toCharArray()	Converts this string to a new character array
+- substring()	Returns a new string which is the substring of a specified string
+
+StringBuilder Methods ->
+- String str = new String("Nadeem");
+- append(String s)	It is utilized to append the specified string to the provided string. This function is overloaded.
+- insert(int offset, String s)	This string is used to insert the provided string at the designated place.
+- delete(int startIndex, int endIndex)	It is used to delete the string from startIndex and endIndex that are provided.
+- reverse()	It is used to reverse the string.
+- charAt(int index)	It is utilised to retrieve the character at the requested location.
+- length()	The length of the string, or the total number of characters, is returned using this method.
+- substring(int beginIndex)	The substring starting at the supplied beginIndex is returned using it.
+- substring(int beginIndex, int endIndex)	It is used to retrieve the substring starting at the beginIndex and endIndex values supplied.
+*/
