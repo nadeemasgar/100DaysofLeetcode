c684cf2cd28c076937884d938fc4925b3dd4c26e
MD NADEEM ASGAR <43141745+nadeemasgar@users.noreply.github.com>
2024-04-01 22:29:34 +0530

    Time: 3 ms (98.23%), Space: 44.7 MB (46.20%) - LeetHub

---
+++ 2810-faulty-keyboard/2810-faulty-keyboard.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String finalString(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(ch != 'i') {
+                sb.append(ch);
+            }
+            else {
+                sb.reverse();
+            }
+        }
+        
+        return sb.toString();
+    }
+}
