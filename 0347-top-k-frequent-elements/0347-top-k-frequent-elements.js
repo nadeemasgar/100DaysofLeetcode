f6a99119d4057d91962336d0830c9526ffa318b8
MD NADEEM ASGAR <43141745+nadeemasgar@users.noreply.github.com>
2024-04-08 13:00:34 +0530

    Time: 71 ms (48.74%), Space: 54.2 MB (46.98%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -9,12 +9,12 @@ var topKFrequent = function(nums, k) {
         const freq = map.get(num) || 0;
         map.set(num, freq + 1);
     }
-    console.log(map)
+
     let count = 0;
     const sortedByValue = new Map([...map.entries()].sort((a, b) => b[1] - a[1]));
 
     const ans = [];
-    for(let [key, value] of sortedByValue) {
+    for(let key of sortedByValue.keys()) {
         if(count < k) {
             ans.push(key);
             count++;
