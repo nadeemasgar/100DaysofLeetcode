7e6272c82bd9813812cb68ec5b65366f82796ec6
MD NADEEM ASGAR <43141745+nadeemasgar@users.noreply.github.com>
2024-04-18 01:11:57 +0530

    Time: 52 ms (87.51%), Space: 48.3 MB (98.74%) - LeetHub

---
+++ 2627-debounce/2627-debounce.js
@@ -0,0 +1,22 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function(fn, t) {
+    let timer = null;
+    return function(...args) {
+        let context = this;
+        if(timer) clearTimeout(timer);
+        timer = setTimeout(() => {
+            fn.call(context, ...args);
+        }, t)
+    }
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
