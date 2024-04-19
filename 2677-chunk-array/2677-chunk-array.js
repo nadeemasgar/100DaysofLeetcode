3d3e441062eda2e0322112ebcbca8a0bee83399f
MD NADEEM ASGAR <43141745+nadeemasgar@users.noreply.github.com>
2024-04-20 00:13:18 +0530

    Time: 45 ms (94.80%), Space: 51.5 MB (59.22%) - LeetHub

--- 2677-chunk-array/2677-chunk-array.js
+++ 2677-chunk-array/2677-chunk-array.js
@@ -4,10 +4,12 @@
  * @return {Array}
  */
 var chunk = function(arr, size) {
+    let idx = 0;
     const result = [];
-    while(arr.length > 0) {
-        const removedEle = arr.splice(0, size);
-        result.push(removedEle);
+    while(idx < arr.length) {
+        const chunkedArray = arr.slice(idx, idx + size);
+        result.push(chunkedArray);
+        idx += size;
     }
     
     return result;
