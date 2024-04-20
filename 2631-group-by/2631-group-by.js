1e0573e31a51328bd810706109db5dd63f69a2ae
MD NADEEM ASGAR <43141745+nadeemasgar@users.noreply.github.com>
2024-04-21 01:18:43 +0530

    Time: 129 ms (39.95%), Space: 73.3 MB (30.91%) - LeetHub

---
+++ 2631-group-by/2631-group-by.js
@@ -0,0 +1,22 @@
+/**
+ * @param {Function} fn
+ * @return {Object}
+ */
+Array.prototype.groupBy = function(fn) {
+    return this.reduce((acc, curr) => {
+        const key = fn(curr);
+   
+        if(!(key in acc)) {
+            acc[key] = []
+        } 
+        
+        acc[key].push(curr);
+        
+        return acc;
+        
+    }, {})
+};
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
