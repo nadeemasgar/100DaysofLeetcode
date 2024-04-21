e717035f3f5081817be5b584310397140ff7e7c9
MD NADEEM ASGAR <43141745+nadeemasgar@users.noreply.github.com>
2024-04-22 02:23:49 +0530

    Time: 294 ms (69.97%), Space: 86.3 MB (90.65%) - LeetHub

--- 2623-memoize/2623-memoize.js
+++ 2623-memoize/2623-memoize.js
@@ -4,14 +4,14 @@
  */
 function memoize(fn) {
     const cache = {};
-
+    
     return function(...args) {
-        const key = JSON.stringify(args); // const key = args.join("-");
+        const key = JSON.stringify(args);
         if(key in cache) {
             return cache[key];
         }
-        cache[key] = fn(...args);
-        return cache[key];
+        
+        return cache[key] = fn(...args);
     }
 }
 
