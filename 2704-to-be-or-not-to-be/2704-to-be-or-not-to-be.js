a89f5053607e1483459a8b476f9527b79ed8fba3
MD NADEEM ASGAR <43141745+nadeemasgar@users.noreply.github.com>
2024-05-09 01:05:55 +0530

    Time: 57 ms (24.93%), Space: 48.7 MB (65.10%) - LeetHub

--- 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -5,13 +5,22 @@
 var expect = function(val1) {
     return {
         toBe: (val2) => {
-            if(val1 === val2) return true;
-            else throw "Not Equal";
+            if(val1 === val2) {
+                return true;
+            }
+            else if(val1 !== val2) {
+                throw new Error("Not Equal");
+            }
         },
         notToBe: (val2) => {
-            if(val1 !== val2) return true;
-            else throw "Equal";
+            if(val1 !== val2) {
+                return true;
+            }
+            else if(val1 === val2) {
+                throw new Error("Equal");
+            }
         }
+        
     }
 };
 
